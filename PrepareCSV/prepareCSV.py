647ac8484d6a9f50d037111e2b0162cf55788408
Arnaud Charleroy <arnaud.charleroy@inra.fr>
2018-11-07 15:10:32 +0100

    Correct some files

--- PrepareCSV/prepareCSV.py
+++ PrepareCSV/prepareCSV.py
@@ -1,29 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-#******************************************************************************
+# ******************************************************************************
 #                                    prepareCSV.py
 # SILEX-PHIS
 # Copyright © INRA 2018
 # Creation date:  07 November, 2018
 # Contact: arnaud.charleroy@inra.fr, anne.tireau@inra.fr, pascal.neveu@inra.fr
-#******************************************************************************
+# ******************************************************************************
 
 import csv
 from tools import utils
 from tools import log
-
+import dateparser
 
 import os
 import re
 import os.path
 
 replacedChars = {
-		" ": "",
-		"'", "",
-		"/", "",
-		"+", ""
-	       }
-			
+    " ": "",
+    "'": "",
+    "/": "",
+    "+": ""
+}
 
 DIRECTORY_PATH = ""
 OUTPUT_DIRECTORY_PATH = ""
@@ -44,30 +43,29 @@ for filePath in filePaths:
             firstline = True
             for row in spamreader:
                 tmpRow = []
-                if(firstline) :
+                if(firstline):
                     for element in row:
                         tmpRow.append(element)
                     rows.append(tmpRow)
-                else :
+                else:
                     for element in row:
-                        element = utils.clean(element,replacedChars)
+                        element = utils.clean(element, replacedChars)
                         tmpRow.append(element)
                     rows.append(tmpRow)
                 firstline = False
-    except :
-        with open(filePath, 'r', encoding = "ISO-8859-1") as csvfile:
+    except:
+        with open(filePath, 'r', encoding="ISO-8859-1") as csvfile:
             spamreader = csv.reader(csvfile, delimiter=',')
             for row in spamreader:
                 tmpRow = []
                 for element in row:
-                    element = utils.clean(element,replacedChars)
+                    element = utils.clean(element, replacedChars)
                     tmpRow.append(element)
                 rows.append(tmpRow)
-    
+
     log.info("Writing")
     print(OUTPUT_DIRECTORY_PATH + utils.getFileNameFromFilePath(filePath))
     with open(OUTPUT_DIRECTORY_PATH + "/" + utils.getFileNameFromFilePath(filePath), 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerows(rows)
     name = + 1
-
--- PrepareCSV/tools/utils.py
+++ PrepareCSV/tools/utils.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-#******************************************************************************
+# ******************************************************************************
 #                                    utils.py
 # SILEX-PHIS
 # Copyright © INRA 2018
 # Creation date:  13 September, 2018
 # Contact: arnaud.charleroy@inra.fr, anne.tireau@inra.fr, pascal.neveu@inra.fr
-#******************************************************************************
+# ******************************************************************************
 
 import os
 import json
@@ -15,8 +15,11 @@ import csv
 import unidecode
 import log as log
 import datetime
+import dateparser
 
-def serializeDate(self,date):
+DATE_FORMAT = '%Y-%m-%d'
+
+def serializeDate(self, date):
     """Date object is not serializable by JsonSerializer by default"""
     # Test if the date is serializable
     if type(date) is datetime.date or type(date) is datetime.datetime:
@@ -24,9 +27,11 @@ def serializeDate(self,date):
     else:
         return date
 
+
 def getExtensionFromFilename(filename):
     extensionWithPoint = os.path.splitext(filename)[1]
     return extensionWithPoint.replace(".", "")
+
 
 def getNameFromFilename(filename):
     extensionWithPoint = os.path.splitext(filename)[0]
@@ -34,6 +39,8 @@ def getNameFromFilename(filename):
 
 # @link https://stackoverflow.com/questions/2212643/python-recursive-folder-read
 # walk recursively in directory
+
+
 def getFileNameRecursively(walk_dir):
     """ Description
     :type walk_dir:
@@ -44,13 +51,14 @@ def getFileNameRecursively(walk_dir):
     :rtype:
     """
     filePaths = []
-    for root,directory_names,file_names in os.walk(walk_dir):
+    for root, directory_names, file_names in os.walk(walk_dir):
         for filename in file_names:
             filePath = os.path.join(root, filename)
             if(os.path.isfile(filePath)):
                 filePaths.append(filePath)
-    log.info(str(len(filePaths)) + " files found" )
+    log.info(str(len(filePaths)) + " files found")
     return filePaths
+
 
 def getFileNameFromFilePath(filePath):
     """ Description
@@ -64,21 +72,28 @@ def getFileNameFromFilePath(filePath):
     head, tail = os.path.split(filePath)
     return tail
 
+
 def removeNewLine(line):
     if(isinstance(line, str)):
         return line.rstrip()
     else:
         return line
 
+
 def readCSV(filePath):
     with open(filePath, 'rb') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
         return spamreader
 
-def clean(element):
+
+def clean(element, replacedChars):
+    element = removeNewLine(element)
+    if(dateparser.parse(element) != None):
+        return dateparser.parse(element).strftime(DATE_FORMAT)
+    else:
         element = removeNewLine(unidecode.unidecode(element))
         element = element.replace(" ", "")
         element = element.replace("'", "")
         element = element.replace("/", "")
         element = element.replace("+", "")
-        return element
+    return element
