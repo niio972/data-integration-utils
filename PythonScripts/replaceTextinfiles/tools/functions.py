7a128f9044470731d061583c03151dc0f642ce92
Arnaud CHARLEROY <neodriva@gmail.com>
2018-12-20 17:27:20 +0100

    Refactoring code

--- PythonScripts/replaceTextinfiles/tools/functions.py
+++ PythonScripts/replaceTextinfiles/tools/functions.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
-#******************************************************************************
+# ******************************************************************************
 #                                    functions.py
 # BigDataGrapes
 # Copyright © INRA 2018
 # Creation date:  20 December, 2018
 # Contact: arnaud.charleroy@inra.fr, anne.tireau@inra.fr, pascal.neveu@inra.fr
-#******************************************************************************
+# ******************************************************************************
 
 
 import os
 import sys
 import yaml
+import re
 from subprocess import call
-from shlex import quote
 
 from tools import log as logger
+
 
 def loadVocabularyFile(filePath):
     """ Load config file """
@@ -44,15 +45,17 @@ def isValidFile(filePath):
         return False
 
 
-def multipleReplace(text, vocabulary):
+def multipleReplace(line, vocabulary):
     """
     take a text and replace words that match the key in a dictionary
     with the associated value, return the changed text
     """
     for uniqTerm in vocabulary:
         for termSearched in vocabulary[uniqTerm]:
-            text = text.replace(termSearched, uniqTerm)
-    return text
+            if(termSearched != None):
+                data = re.sub("('',","", data)
+                line = line.replace(termSearched, uniqTerm)
+
 
 def runReplacing(args):
     inputDirectoryPath = args.i
@@ -70,7 +73,8 @@ def runReplacing(args):
             logger.info("Creating output directory :" + outputDirectoryPath)
             os.makedirs(outputDirectoryPath, exist_ok=True)
             ValidOutputDirectory = True
-            logger.info("Directory created successfully :" + outputDirectoryPath)
+            logger.info("Directory created successfully :" +
+                        outputDirectoryPath)
         except OSError as exc:  # Guard against race condition
             logger.err(outputDirectoryPath + " is not a valid directory")
 
@@ -80,7 +84,6 @@ def runReplacing(args):
     if(not ValidInputDirectory or not ValidOutputDirectory or not ValidVocabularyFile):
         sys.exit()
 
-
     AbsInputDirectoryPath = os.path.abspath(inputDirectoryPath)
     AbsOutputDirectoryPath = os.path.abspath(outputDirectoryPath)
     filenameList = os.listdir(os.path.abspath(AbsInputDirectoryPath))
@@ -89,14 +92,8 @@ def runReplacing(args):
     for filename in filenameList:
         inputFilename = os.path.join(AbsInputDirectoryPath, filename)
         outputFile = os.path.join(AbsOutputDirectoryPath, filename)
+        fileIn = open(inputFilename, 'r')
+        data = fileIn.read()
+        fileIn.close()
+        data
 
-        # Read in the file
-        with open(inputFilename, 'r') as file:
-            filedata = file.read()
-
-        # Replace the target string
-        filedata = multipleReplace(filedata, vocabulary)
-
-        # Write the file out again
-        with open(outputFile, 'w') as file:
-            file.write(filedata)
